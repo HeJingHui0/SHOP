1581428addeec73a0d4903ef1dc87753c6d2c7ba
HEJINGHUI0 <q1036849399@163.com>
2020-05-16 21:59:07 +0800

    login组件更新

---
+++ src/components/login.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="login_container">
+        <div class="login_box">
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="">
+            </div>
+            <el-form label-width="0px" class="login_form">
+                <el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+                <el-form-item class="btns">
+                    <el-button type="primary">登录</el-button>
+                    <el-button type="warning">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: lightblue;
+    height: 100%;
+}
+.login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    .avatar_box {
+        width: 100px;
+        height: 100px;
+        border: 1px solid #eee;
+        border-radius: 50%;
+        padding: 10px;
+        box-shadow: 0 0 10px #ddd;
+        background-color: #fff;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: #eee;
+        }
+    }
+}
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'element-ui/lib/theme-chalk/index.css' //解决由于<style lang="less" scoped>中scoped引起的样式失效问题
+import ElementUi from 'element-ui'
+import './assets/css/global.css'
+Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
