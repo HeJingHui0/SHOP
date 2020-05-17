9dfaa1a7b31135c2bf66e5b1ab14cd5c4eb4b8c8
HEJINGHUI0 <q1036849399@163.com>
2020-05-17 14:53:24 +0800

    主页侧边栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,128 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <el-header>
+      <div>
+        <img src="../assets/img/img.jpg" alt />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle_button" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+        >
+          <el-submenu :index="`${item.id}`" v-for="item in menuList" :key="item.id">
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menuList: [],
+      //菜单图标
+      iconsObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      isCollapse: false
+    }
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status != 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
     }
+  },
+  created() {
+    this.getMenuList()
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home_container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373e4d;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    img {
+      width: 70px;
+      border-radius: 50%;
+    }
+    span {
+      margin-left: 20px;
+    }
+  }
+}
+.el-aside {
+  background-color: #545c64;
+  .el-menu {
+    border-right: none;
+  }
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 5px;
+}
+.toggle_button {
+  background-color: #2c303d;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.4em;
+  cursor: pointer;
+}
 </style>
