a10a24abb9140ad6b2a27512de835f86c975b321
HEJINGHUI0 <q1036849399@163.com>
2020-05-19 20:43:53 +0800

    添加用户完成

--- src/components/user/Uesrs.vue
+++ src/components/user/Uesrs.vue
@@ -13,7 +13,7 @@
           </el-input>
         </el-col>
         <el-col :span="4">
-          <el-button type="primary">添加用户</el-button>
+          <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
         </el-col>
       </el-row>
       <el-table :data="userList" border stripe>
@@ -51,12 +51,46 @@
         :total="total"
       ></el-pagination>
     </el-card>
+    <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClose">
+      <el-form :model="addInfo" :rules="addInfoRules" ref="addInfoRef" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addInfo.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addInfo.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addInfo.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addInfo.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUserInfo">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   data() {
+    let checkEmail = (rule, value, callback) => {
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入合法的邮箱'))
+    }
+    let checkMobile = (rule, value, callback) => {
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[0123456789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+      if (regMobile.test(value)) {
+        return callback()
+      }
+      callback(new Error('请输入合法的手机号'))
+    }
     return {
       queryInfo: {
         query: '',
@@ -64,7 +98,32 @@ export default {
         pagesize: 2
       },
       userList: [],
-      total: 0
+      total: 0,
+      addDialogVisible: false,
+      addInfo: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      addInfoRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '用户名格式错误', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '密码格式错误', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -77,21 +136,38 @@ export default {
       this.userList = res.data.users
       this.total = res.data.total
     },
-    handleSizeChange(newSize){
+    handleSizeChange(newSize) {
       this.queryInfo.pagesize = newSize
       this.getUserList()
     },
-    handleCurrentChange(newNum){
+    handleCurrentChange(newNum) {
       this.queryInfo.pagenum = newNum
       this.getUserList()
     },
-    async userStateChange(userInfo){
-      const {data: res} = await this.$http.put(`users/${userInfo.id}/state/${userInfo.mg_state}`)
-      if (res.meta.status !== 200){
+    async userStateChange(userInfo) {
+      const { data: res } = await this.$http.put(
+        `users/${userInfo.id}/state/${userInfo.mg_state}`
+      )
+      if (res.meta.status !== 200) {
         userInfo.mg_state = !userInfo.mg_state
         return this.$message.error('更新状态失败')
       }
       this.$message.success('更新状态成功')
+    },
+    addDialogClose() {
+      this.$refs.addInfoRef.resetFields()
+    },
+    addUserInfo() {
+      this.$refs.addInfoRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('users', this.addInfo)
+        if (res.meta.status !== 201) {
+          this.$message.error('添加失败')
+        }
+        this.$message.success('添加成功')
+        this.addDialogVisible = false
+        this.getUserList()
+      })
     }
   },
   created() {
