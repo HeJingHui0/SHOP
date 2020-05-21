f82a5d44f66ec9c1c8468592f9c25144c60b7ae7
HEJINGHUI0 <q1036849399@163.com>
2020-05-21 21:37:08 +0800

    权限列表展示

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Uesrs.vue'
+import Rights from '../components/power/Rights.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,7 @@ const router = new Router({
       children:[
         {path: '/welcome', component: Welcome},
         {path: '/users', component: Users},
+        {path: '/rights', component: Rights}
     ]},
   ]
 })
