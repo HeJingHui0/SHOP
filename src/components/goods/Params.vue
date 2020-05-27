d1e38c8934c93e1be25e2fb032ec5fb8c023c610
HEJINGHUI0 <q1036849399@163.com>
2020-05-27 20:49:20 +0800

    商品添加参数完成

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -23,7 +23,12 @@
       </el-row>
       <el-tabs v-model="activeName" @tab-click="handleParamsClick">
         <el-tab-pane label="动态参数" name="many">
-          <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加参数</el-button>
+          <el-button
+            type="primary"
+            size="mini"
+            :disabled="isBtnDisabled"
+            @click="addDialogVisible = true"
+          >添加参数</el-button>
           <el-table :data="manyParamsData" border stripe>
             <el-table-column type="expand"></el-table-column>
             <el-table-column type="index"></el-table-column>
@@ -37,7 +42,12 @@
           </el-table>
         </el-tab-pane>
         <el-tab-pane label="静态属性" name="only">
-          <el-button type="primary" size="mini" :disabled="isBtnDisabled">添加属性</el-button>
+          <el-button
+            type="primary"
+            size="mini"
+            :disabled="isBtnDisabled"
+            @click="addDialogVisible = true"
+          >添加属性</el-button>
           <el-table :data="onlyParamsData" border stripe>
             <el-table-column type="expand"></el-table-column>
             <el-table-column type="index"></el-table-column>
@@ -52,6 +62,28 @@
         </el-tab-pane>
       </el-tabs>
     </el-card>
+    <el-dialog
+      :title="'添加' + titleText"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="paramsDialogClosed"
+    >
+      <!-- 添加动态参数和静态属性共用同一对话框 -->
+      <el-form
+        :model="addParamsForm"
+        :rules="addParamsRules"
+        ref="addParamsRef"
+        label-width="100px"
+      >
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="addParamsForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addParamsInfo">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -68,7 +100,16 @@ export default {
       selectedParamsKeys: [],
       activeName: 'many',
       manyParamsData: [],
-      onlyParamsData: []
+      onlyParamsData: [],
+      addDialogVisible: false,
+      addParamsForm: {
+        attr_name: ''
+      },
+      addParamsRules: {
+        attr_name: [
+          { required: true, message: '请输入参数名称', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
@@ -99,12 +140,34 @@ export default {
       if (res.meta.status !== 200) {
         return this.$message.error('获取参数列表失败')
       }
-      console.log(res.data)
       if (this.activeName === 'many') {
         this.manyParamsData = res.data
       } else {
         this.onlyParamsData = res.data
       }
+    },
+    paramsDialogClosed() {
+      this.$refs.addParamsRef.resetFields()
+    },
+    addParamsInfo() {
+      this.$refs.addParamsRef.validate(async valid => {
+        if (!valid) {
+          return this.$message.error('请输入合法的参数')
+        }
+        const { data: res } = await this.$http.post(
+          `categories/${this.paramsId}/attributes`,
+          {
+            attr_name: this.addParamsForm.attr_name,
+            attr_sel: this.activeName
+          }
+        )
+        if (res.meta.status !== 201) {
+          return this.$message.error('添加参数失败')
+        }
+        this.$message.success('添加参数成功')
+        this.addDialogVisible = false
+        this.getParamsData()
+      })
     }
   },
   created() {
@@ -122,6 +185,12 @@ export default {
         return this.selectedParamsKeys[2]
       }
       return null
+    },
+    titleText() {
+      if (this.activeName === 'many') {
+        return '动态参数'
+      }
+      return '静态属性'
     }
   }
 }
