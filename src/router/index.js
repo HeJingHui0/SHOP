37bad0897b17f427d4c05a700f47a5458cdad68f
HEJINGHUI0 <q1036849399@163.com>
2020-06-01 23:45:57 +0800

    订单管理完成

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import Goods from '../components/goods/Goods.vue'
 import AddGoods from '../components/goods/AddGoods.vue'
+import Order from '../components/order/Order.vue'
 
 Vue.use(Router)
 
@@ -29,7 +30,8 @@ const router = new Router({
         {path: '/categories', component: Cate},
         {path: '/params', component: Params},
         {path: '/goods', component: Goods},
-        {path: '/goods/add', component: AddGoods}
+        {path: '/goods/add', component: AddGoods},
+        {path: '/orders', component: Order}
     ]},
   ]
 })
