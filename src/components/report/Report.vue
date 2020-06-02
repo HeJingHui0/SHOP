101b1e62f0931d33abc4fa4a4694e8cdbc8a90cc
HEJINGHUI0 <q1036849399@163.com>
2020-06-02 23:23:31 +0800

    数据统计完成

---
+++ src/components/report/Report.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 800px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  data() {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  methods: {},
+  created() {},
+  async mounted() {
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取数据报表失败')
+    }
+    // const result = Object.assign(res.data, this.options)
+    //res.data与this.options部分属性合并
+    res.data.title = this.options.title
+    res.data.tooltip = this.options.tooltip
+    res.data.grid = this.options.grid
+    myChart.setOption(res.data)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
